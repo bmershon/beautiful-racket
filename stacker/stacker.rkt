69f4e97a79f268d97dc1f6ebbbfb13cb63bbb17d
Brooks Mershon <brooks@sketchup.com>
2017-06-04 11:46:34 -0600

    whoopee made my first programming language!

--- stacker/stacker.rkt
+++ stacker/stacker.rkt
@@ -10,5 +10,28 @@
 
 (define-macro (stacker-module-begin HANDLE-EXPR ...)
   #'(#%module-begin
-     'HANDLE-EXPR ...))
+     HANDLE-EXPR ...
+     (display (pop-stack!))))
+
+; Rename export.
 (provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+(provide handle)
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
+
+(provide + - * /)
