f5ce85b6844ceec5bae59fddb99ceed8a7d33efb
Brooks Mershon <brooks@sketchup.com>
2017-06-08 22:53:05 -0600

    inline unit tests (to be moved) and begin source location tracking

--- jsonic/test.rkt
+++ jsonic/test.rkt
@@ -1,1 +1,13 @@
-#lang br
+#lang jsonic
+[
+  null,
+  @$ (exact->inexact (/ 3 5)) $@,
+  true,
+  ["array", "of", "strings"],
+  {
+    "key-1": null,
+    "key-2": false,
+    "key-3": {"subkey": 21}
+  },
+  @$ (= 2 (+ 1 1)) $@
+]
--- packages/jsonic/tokenizer.rkt
+++ packages/jsonic/tokenizer.rkt
@@ -1,5 +1,17 @@
 #lang br/quicklang
-(require brag/support)
+(require brag/support racket/contract)
+
+(module+ test
+  (require rackunit))
+
+(define (jsonic-token? x)
+  (or (eof-object? x) (string? x) (token-struct? x)))
+
+(module+ test
+  (check-true (jsonic-token? eof))
+  (check-true (jsonic-token? "a string"))
+  (check-true (jsonic-token? (token 'A-TOKEN-STRUCT "hi")))
+  (check-false (jsonic-token? 42)))
 
 (define (make-tokenizer port)
   (define (next-token)
@@ -10,6 +22,20 @@
        [(from/to "@$" "$@")
         (token 'SEXP-TOK (trim-ends "@$" lexeme "$@"))]
        [any-char (token 'CHAR-TOK lexeme)]))
-    (jsonic-lexer port)) 
+    (jsonic-lexer port))
   next-token)
-(provide make-tokenizer)
+(provide
+ (contract-out
+  [make-tokenizer (input-port? . -> . (-> jsonic-token?))]))
+
+(module+ test
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "// comment\n")
+   empty)
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "@$ (+ 6 7) $@")
+   (list (token-struct 'SEXP-TOK " (+ 6 7) " #f #f #f #f #f)))
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "hi")
+   (list (token-struct 'CHAR-TOK "h" #f #f #f #f #f)
+         (token-struct 'CHAR-TOK "i" #f #f #f #f #f))))
