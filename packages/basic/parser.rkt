5603a7e80376c40e7cb4bfa65f738d718fc28d9a
Brooks Mershon <brooks@sketchup.com>
2017-06-18 14:17:08 -0600

    get the rest of the first basic tutorial working

--- packages/basic/parser.rkt
+++ packages/basic/parser.rkt
@@ -4,8 +4,8 @@ b-line : b-line-num [b-statement] (/":" [b-statement])* [b-rem]
 @b-line-num : INTEGER
 @b-statement : b-end | b-print | b-goto
 b-rem : REM
-b-end : "end"
-b-print : "print" [b-printable] (/";" [b-printable])*
+b-end : /"end"
+b-print : /"print" [b-printable] (/";" [b-printable])*
 @b-printable : STRING | b-expr
 b-goto : /"goto" b-expr
 b-expr : b-sum
